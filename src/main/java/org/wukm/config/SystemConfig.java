c5021e58915c6501503e1c35e775ae0a2d4f94bd
wukm <wukunmeng@xinpianchang.com>
2018-04-28 17:46:37 +0800

    test

--- src/main/java/org/wukm/config/SystemConfig.java
+++ src/main/java/org/wukm/config/SystemConfig.java
@@ -10,8 +10,16 @@
  */
 package org.wukm.config;
 
+import com.jfinal.aop.Duang;
 import com.jfinal.config.*;
 import com.jfinal.template.Engine;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 /**
  * Create with IntelliJ IDEA
@@ -25,9 +33,18 @@ import com.jfinal.template.Engine;
  * To change this template use File | Settings | File and Code Templates.
  */
 public class SystemConfig extends JFinalConfig {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public void configConstant(Constants me) {
-
+        try {
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("mybatis-config.xml"));
+            sqlSessionFactory.openSession();
+            logger.error("sqlSessionFactory:{}", sqlSessionFactory);
+        } catch (IOException e){
+            logger.error("IOException:{}", e.getMessage());
+        }
     }
 
     @Override
