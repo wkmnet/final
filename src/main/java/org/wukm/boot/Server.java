0dcd121edb14533826a4f276ce0a7ed7ceee51de
wukm <wukunmeng@xinpianchang.com>
2018-06-08 10:56:31 +0800

    server start config

--- src/main/java/org/wukm/boot/Server.java
+++ src/main/java/org/wukm/boot/Server.java
@@ -11,12 +11,16 @@
 package org.wukm.boot;
 
 import com.jfinal.server.JettyServerForIDEA;
+import org.apache.commons.lang.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Properties;
 
 /**
  * Create with IntelliJ IDEA
@@ -34,8 +38,36 @@ public class Server {
     private static Logger logger = LoggerFactory.getLogger(Server.class);
 
     public static void main(String[] args){
+        logger.info("ROOT DIR:{}", System.getProperty("user.dir"));
         Path root = Paths.get(System.getProperty("user.dir"),"src/main/webapp");
-        logger.info("ROOT:{}", root.toUri().toString());
-        new JettyServerForIDEA(root.toUri().toString(),8080,"/").start();
+        if(root.toFile().exists()) {
+            logger.info("WebApp:{}", root.toUri().toString());
+        } else {
+            root = Paths.get(System.getProperty("user.dir"),"webapp");
+            logger.info("WebApp:{}", root.toUri().toString());
+        }
+        Path config = Paths.get(System.getProperty("user.dir"),"conf/server.properties");
+        File file = config.toFile();
+        if(file.exists() && file.isFile()) {
+            logger.info("find config:{}", config.toString());
+            Properties properties = new Properties();
+            try {
+                properties.load(new FileReader(file));
+            } catch (IOException ignore){
+                //忽略读取配置文件异常
+                logger.info("server context:{} port:{}", "/", 8080);
+                new JettyServerForIDEA(root.toUri().toString(), 8080, "/").start();
+                return;
+            }
+            String context = properties.getProperty("server.context","/");
+            String configPort = properties.getProperty("server.port","8080");
+            int port = NumberUtils.toInt(configPort,8080);
+            logger.info("server context:{} port:{}", context, port);
+            new JettyServerForIDEA(root.toUri().toString(), port, context).start();
+        } else {
+            logger.info("not find config:{}", config.toString());
+            logger.info("server context:{} port:{}", "/", 8080);
+            new JettyServerForIDEA(root.toUri().toString(), 8080, "/").start();
+        }
     }
 }
